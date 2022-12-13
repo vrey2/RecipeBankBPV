1f51c11b9b8c441574f3a778489b1c3736edc486
Ismael F <leamsifigs@users.noreply.github.com>
2022-12-12 22:00:59 -0800

    starting point

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_bootstrap import Bootstrap
 from app.api.routes import api
 from app.site.routes import site
 from app.admin.routes import admin
+
+from app.extensions import db
 def booty(app):
     boot = Bootstrap(app)
     return boot
@@ -20,6 +22,15 @@ def create_app():
     app.register_blueprint(admin)
 
     app.config['SECRET_KEY'] = 'secretKey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    # thios is only here cause i was testing. this makes DB but does not build.
+    with app.app_context():
+        db.create_all()
+
 
     return app
 
--- app/site/routes.py
+++ app/site/routes.py
@@ -4,6 +4,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from app.extensions import db
 
 site = Blueprint('site', __name__, template_folder='templates')
 
