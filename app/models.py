3a8e0aad81f23dafd44c9f7a7845d86b35d74eec
Ismael F <leamsifigs@users.noreply.github.com>
2022-12-13 04:49:56 -0800

    commenting out complex models

--- app/models.py
+++ app/models.py
@@ -8,15 +8,15 @@ class User(db.Model, UserMixin):
     username = db.Column('username', db.String(20), unique=True)
     password = db.Column('password', db.String(20))
 
-class Recipe(db.Model):
-    __tablename__ = 'Recipes'
-    id = db.Column('id', db.Integer, primary_key= True)
-    name = db.Column('name', db.String(150))
-    serving = db.Column('serve', db.Integer())
-    prep = db.Column('prep', db.String(100))
-    cook = db.Column('cook', db.String(100))
-    ingredients = db.Column('ingr', db.String(1000))
-    instruction = db.Column('instr', db.String(1000))
-    time = db.Column('time', db.String(100))
-    user_id = db.Column('user_id', db.ForeignKey('user.id'), nullable=False)
+#class Recipe(db.Model):
+#    __tablename__ = 'Recipes'
+#    id = db.Column('id', db.Integer, primary_key= True)
+#    name = db.Column('name', db.String(150))
+#    serving = db.Column('serve', db.Integer())
+#    prep = db.Column('prep', db.String(100))
+#    cook = db.Column('cook', db.String(100))
+#    ingredients = db.Column('ingr', db.String(1000))
+#    instruction = db.Column('instr', db.String(1000))
+#    time = db.Column('time', db.String(100))
+#    user_id = db.Column('user_id', db.ForeignKey('user.id'), nullable=False)
     # author = db.relationship('User', backref=db.backref('Recipe', uselist=False))
