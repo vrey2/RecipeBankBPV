b843b1d80c94055ae534437ff98628d2d0d7a219
Ismael F <leamsifigs@users.noreply.github.com>
2022-12-14 09:42:34 -0800

    add basic functionality for home, profile

--- app/site/routes.py
+++ app/site/routes.py
@@ -4,28 +4,38 @@ from flask_wtf import FlaskForm, form
 from flask_sqlalchemy import SQLAlchemy, query
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Length, ValidationError
-from flask_bcrypt import Bcrypt, generate_password_hash
+from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
 from flask_login import logout_user, login_required, current_user, login_user
 
-from app.api.routes import getrecipes
+from app.api.routes import getrecipes, getuserrecipes
 from app.extensions import db, logMan, bcrypt
 from app.models import User
 site = Blueprint('site', __name__, template_folder='templates')
 
-@site.route('/', methods=['GET', 'POST'])
+@site.route('/', methods=['GET'])
+def index():
+    return render_template('site/index.html')
+
+@site.route('/login', methods=['GET', 'POST'])
 def login():
     # return render_template('/empty.html')
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+        print(user)
+        print("submit")
         if user:
-            if user.password == form.password.data:
+            print("if user")
+            if check_password_hash(user.password, form.password.data):
+                print("password correct")
                 login_user(user)
-                return redirect('site.home', user=user)
+                return redirect(url_for('site.home'))
     else:
+        print("not authing")
         form = LoginForm()
         return render_template('site/login.html', form=form)
-    return render_template('site/home.html', form=form)
+
+    return render_template('site/home.html', form=form, user=current_user)
 
 @site.route('/register', methods=['GET', 'POST'])
 def register():
@@ -49,12 +59,16 @@ def logout():
 @site.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template('site/home.html', user=current_user, items=getrecipes())
+    reclist = getrecipes()
+    print("this is getrec")
+    print(reclist)
+    return render_template('site/home.html', current_user=current_user, items=reclist)
 
 @site.route('/profile', methods=['GET', 'POST', 'PUT'])
 @login_required
 def profile():
-    return render_template('site/profile.html')
+    getusrrec = getuserrecipes(current_user.username)
+    return render_template('site/profile.html', current_user=current_user, items=getusrrec)
 
 
 class LoginForm(FlaskForm):
