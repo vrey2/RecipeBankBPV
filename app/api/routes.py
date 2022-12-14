80e02bc11aa5d8c22e5e24fe94ec757a33a7ead8
Ismael F <leamsifigs@users.noreply.github.com>
2022-12-13 21:55:05 -0800

    added faker, created fakeDB builder

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,8 +1,27 @@
 from flask import Blueprint, render_template
+from ..extensions import db
+from ..models import *
+from faker import Faker
+from faker_sqlalchemy import SqlAlchemyProvider
+
+fake = Faker()
+fake.add_provider(SqlAlchemyProvider)
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 @api.route('/getrecipes')
 def getrecipes():
-    return print('data')
+    allData = Recipe.query.all()
+    return allData
 
+@api.route('/buildFakeDB')
+def buildFakeDB():
+    mike1 = User(username=fake.first_name(), password=fake.ssn())
+    db.session.add(mike1)
+    instance = fake.sqlalchemy_model(Recipe)
+    instance.author = mike1.username
+    db.session.add(instance)
+    db.session.commit()
+    print(mike1)
+    print(instance)
+    return "done!"
