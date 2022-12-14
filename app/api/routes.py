eb173e5a5a4dd796d98963932c6a55fa3af0765a
Vianca <reynosovianca@gmail.com>
2022-12-14 02:11:34 -0800

    Revert "?"
    This reverts commit b727d1a0ec386d5b81b7692b42db6e3035722702.

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,27 +1,8 @@
 from flask import Blueprint, render_template
-from ..extensions import db
-from ..models import *
-from faker import Faker
-from faker_sqlalchemy import SqlAlchemyProvider
-
-fake = Faker()
-fake.add_provider(SqlAlchemyProvider)
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 @api.route('/getrecipes')
 def getrecipes():
-    recipes = db.session.execute(db.select(Recipe).order_by(Recipe.id)).scalars()
-    return recipes.all()
+    return print('data')
 
-@api.route('/buildFakeDB')
-def buildFakeDB():
-    mike1 = User(username=fake.first_name(), password=fake.ssn())
-    db.session.add(mike1)
-    instance = fake.sqlalchemy_model(Recipe)
-    instance.author = mike1.username
-    db.session.add(instance)
-    db.session.commit()
-    print(mike1)
-    print(instance)
-    return "done!"
--- app/site/routes.py
+++ app/site/routes.py
@@ -1,14 +1,10 @@
-
+import bcrypt
 from flask import Blueprint, render_template, url_for,redirect
-from flask_wtf import FlaskForm, form
-from flask_sqlalchemy import SQLAlchemy, query
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Length, ValidationError
-from flask_bcrypt import Bcrypt, generate_password_hash
-from flask_login import logout_user, login_required, current_user, login_user
-from app.extensions import db, logMan, bcrypt
-from app.models import User
-from app.api.routes import api
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
 
 site = Blueprint('site', __name__, template_folder='templates')
 
@@ -17,8 +13,8 @@ def login():
     # return render_template('/empty.html')
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        return render_template('site/home.html', user=user)
+        # user = User.query.filter_by(username=form.username.data).first()
+        return redirect(url_for('site/home.html'))
 
     return render_template('site/login.html', form=form)
 
@@ -26,46 +22,15 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        new_pass = bcrypt.generate_password_hash(form.password.data)
+        new_pass = bcrypt.gensalt(form.password.data)
         new_user = User(username=form.username.data, password=new_pass)
-        db.session.add(new_user)
-        db.session.commit()
-        form_1 = LoginForm()
-        return render_template('site/login.html', form=form_1)
+
+        return render_template('site/login.html')
     return render_template('site/register.html', form=form)
 class User(db.Model, UserMixin):
     id = db.Column('id', db.Integer, primary_key=True)
     username = db.Column('username', db.String(20), unique=True)
     password = db.Column('password', db.String(20))
-
-@site.route('/logout', methods=['GET', 'POST'])
-def logout():
-    logout_user()
-    form = LoginForm()
-    return render_template('site/login.html', form=form)
-
-@site.route('/home', methods=['GET', 'POST'])
-def home():
-
-    return render_template('site/home.html')
-
-@site.route('/profile', methods=['GET', 'POST', 'PUT'])
-def profile():
-
-    return render_template('site/profile.html', user=user)
-
-@site.route('/getrecipes')
-def getrecipes():
-    return 'hello world'
-
-@site.route('/myrecipes', methods=['GET', 'POST', 'PUT'])
-def myrecipes():
-    return 'here are the recipes'
-
-@logMan.user_loader
-def load_user(user_id):
-    user = User.query.get_id(int(user_id))
-    return user
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
@@ -78,6 +43,6 @@ class RegisterForm(FlaskForm):
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField('Register')
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(username=username.data).first()
+        # existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
             raise ValidationError('That username already exists. Please choose a different one.')
