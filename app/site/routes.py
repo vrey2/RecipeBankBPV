d4c5862e33fb86402d09ba1719c55e839270759b
Ismael F <leamsifigs@users.noreply.github.com>
2022-12-14 10:45:24 -0800

    add_record and update routes

--- app/site/routes.py
+++ app/site/routes.py
@@ -2,14 +2,15 @@ import flask_login
 from flask import Blueprint, render_template, url_for,redirect
 from flask_wtf import FlaskForm, form
 from flask_sqlalchemy import SQLAlchemy, query
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, HiddenField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
 from flask_login import logout_user, login_required, current_user, login_user
 
 from app.api.routes import getrecipes, getuserrecipes
 from app.extensions import db, logMan, bcrypt
-from app.models import User
+from app.models import User, Recipe
+
 site = Blueprint('site', __name__, template_folder='templates')
 
 @site.route('/', methods=['GET'])
@@ -70,6 +71,27 @@ def profile():
     getusrrec = getuserrecipes(current_user.username)
     return render_template('site/profile.html', current_user=current_user, items=getusrrec)
 
+@site.route('/add_record', methods=['GET', 'POST'])
+def add_record():
+    form1 = AddRecord()
+    if form1.validate_on_submit():
+        name = form1.name
+        serve = form1.serve
+        prep = form1.prep
+        cook = form1.cook
+        ingr = form1.ingr
+        instr = form1.instr
+        time = form1.time
+        author = form1.author
+        record = Recipe(name=name.data, serve=serve.data, prep=prep.data, cook=cook.data, ingr=ingr.data, instr=instr.data, time=time.data, author=author.data)
+        # Flask-SQLAlchemy magic adds record to database
+        db.session.add(record)
+        db.session.commit()
+        # create a message to send to the template
+        message = f"Recipe \"{name.data}\" for  {author.data} has been submitted."
+        return render_template('site/add_record.html', message=message, form1=form1)
+    else:
+        return render_template('site/add_record.html', form1=form1)
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
@@ -85,3 +107,18 @@ class RegisterForm(FlaskForm):
         existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
             raise ValidationError('That username already exists. Please choose a different one.')
+
+class AddRecord(FlaskForm):
+    # id used only by update/edit
+    id_field = HiddenField()
+    name = StringField('Recipe name')
+    serve = IntegerField('Servers')
+    prep = StringField('Prep Time')
+    cook = StringField('Cooking Time')
+    ingr = StringField('Ingredients')
+    instr = StringField('Instructions')
+    time = StringField('Time to cook')
+    author = StringField('Author (exact username)')
+    # updated - date - handled in the route function
+    updated = HiddenField()
+    submit = SubmitField('Add Record')
