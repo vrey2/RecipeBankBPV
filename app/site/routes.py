5740e0e16950eda53f404fc1a1e75d5503ecc960
Ismael F <leamsifigs@users.noreply.github.com>
2022-12-14 03:25:40 -0800

    restore site/routes
    lmao

--- app/site/routes.py
+++ app/site/routes.py
@@ -1,10 +1,15 @@
-import bcrypt
+----/sites/routes-----
+
+
 from flask import Blueprint, render_template, url_for,redirect
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, form
+from flask_sqlalchemy import SQLAlchemy, query
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Length, ValidationError
-from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt, generate_password_hash
+from flask_login import logout_user, login_required, current_user, login_user
+from app.extensions import db, logMan, bcrypt
+from app.models import User
 
 site = Blueprint('site', __name__, template_folder='templates')
 
@@ -13,8 +18,8 @@ def login():
     # return render_template('/empty.html')
     form = LoginForm()
     if form.validate_on_submit():
-        # user = User.query.filter_by(username=form.username.data).first()
-        return redirect(url_for('site/home.html'))
+        user = User.query.filter_by(username=form.username.data).first()
+        return render_template('site/home.html', user=user)
 
     return render_template('site/login.html', form=form)
 
@@ -22,15 +27,32 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        new_pass = bcrypt.gensalt(form.password.data)
+        new_pass = bcrypt.generate_password_hash(form.password.data)
         new_user = User(username=form.username.data, password=new_pass)
-
-        return render_template('site/login.html')
+        db.session.add(new_user)
+        db.session.commit()
+        form_1 = LoginForm()
+        return render_template('site/login.html', form=form_1)
     return render_template('site/register.html', form=form)
-class User(db.Model, UserMixin):
-    id = db.Column('id', db.Integer, primary_key=True)
-    username = db.Column('username', db.String(20), unique=True)
-    password = db.Column('password', db.String(20))
+
+@site.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    form = LoginForm()
+    return render_template('site/login.html', form=form)
+
+@site.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('site/home.html')
+
+@site.route('/profile', methods=['GET', 'POST', 'PUT'])
+def profile():
+    return render_template('site/profile.html')
+
+@logMan.user_loader
+def load_user(user_id):
+    user = User.query.get_id(int(user_id))
+    return user
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
@@ -43,6 +65,6 @@ class RegisterForm(FlaskForm):
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField('Register')
     def validate_username(self, username):
-        # existing_user_username = User.query.filter_by(username=username.data).first()
+        existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
             raise ValidationError('That username already exists. Please choose a different one.')
